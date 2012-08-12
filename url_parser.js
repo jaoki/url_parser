c846b9b539b395d7d30709955f16310b3dfea83a
jun aoki <jun.aoki.0713@gmail.com>
2012-08-11 23:28:47 -0700

    some files

--- url_parser.js
+++ url_parser.js
@@ -1,2 +1,17 @@
-var git_parser = {};
+function url_parser(url){
+	this.url = url;
+};
+
+url_parser.prototype.url = function() {
+    return this.url;
+};
+
+url_parser.prototype.parse = function() {
+	if(url === undefined || url === null )
+		return {};
+
+	var queryStart = url.indexOf("?");
+};
+
+
 
